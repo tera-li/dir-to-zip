160cf20257f9b1ea476f8c1c3fd806c51b747ec3
ljl <776745393@qq.com>
2021-04-28 22:35:26 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export function qq(e) {
+    return `这是qq函数`
+}
+export function qqq(e) {
+    return `这是qqq函数`
+}
---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+declare module czjs {
+    export function qq(params: string): string
+    export function qqq(params: string): string
+}
+export = czjs
